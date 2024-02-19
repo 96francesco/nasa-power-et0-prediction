77ee4b504c18518b3b8acf349e202eeac835b38e
96francesco <88101466+96francesco@users.noreply.github.com>
2024-02-19 15:46:25 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prediction of reference evapotranspiration (ET0) using the POWER NASA dataset',
+    author='Francesco Pasanisi',
+    license='MIT',
+)

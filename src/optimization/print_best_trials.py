99aa912bf58858051b61388ebd030b0f23302505
96francesco <88101466+96francesco@users.noreply.github.com>
2024-04-05 11:27:13 +0200

    fix import

--- src/optimization/print_best_trials.py
+++ src/optimization/print_best_trials.py
@@ -4,7 +4,7 @@ import optuna
 db_file_path = 'sqlite:////home/francesco/nasa_power_et0_prediction/src/optimization/optimization.db'
 
 # load the study
-study = optuna.load_study(study_name='mlp_optimization', storage=db_file_path)
+study = optuna.load_study(study_name='rf_optimization', storage=db_file_path)
 
 # retrieve the best trials
 pareto_front_trials = study.best_trials
